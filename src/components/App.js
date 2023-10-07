82712ed7c9c44d0a6ad104ffb0090cac570591e0
Maricarmen <nemracira1718@gmail.com>
2023-10-06 19:38:17 -0500

    correccion rutas

--- src/components/App.js
+++ src/components/App.js
@@ -32,10 +32,8 @@ function App() {
 },[])
 
 function handleCardLike(card) {
-  // Verifica una vez más si a esta tarjeta ya le han dado like
   const isLiked = card.likes.some(i => i._id === currentUser._id);
   
-  // Envía una petición a la API y obtén los datos actualizados de la tarjeta
   api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
       setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
   });
